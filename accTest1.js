9bbfa2f3c007ceccfc9d43b03eb61e79aa9fcde1
LoneMarianne <loneoesterlund@gmail.com>
2016-12-30 22:59:30 +0100

    Ny js fil

---
+++ accTest1.js
@@ -0,0 +1,14 @@
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+}
+
+function onError() {
+    alert('onError!');
+}
+
+var options = { frequency: 1000 };  // Update every 3 seconds
+
+var watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
